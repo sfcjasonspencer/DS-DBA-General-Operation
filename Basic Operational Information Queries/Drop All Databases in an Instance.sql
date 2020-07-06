b8717e8db967fe52d20272ddf88b1f144c35be9f
sfcjasonspencer <47889930+sfcjasonspencer@users.noreply.github.com>
2020-07-06 13:44:31 -0400

    l

--- Basic Operational Information Queries/Drop All Databases in an Instance.sql
+++ Basic Operational Information Queries/Drop All Databases in an Instance.sql
@@ -1,5 +1,16 @@
-DECLARE @sql NVARCHAR(max)=''
+--DECLARE @sql NVARCHAR(max)=''
 
-SELECT @sql += ' Drop table ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '
-FROM   INFORMATION_SCHEMA.TABLES
-WHERE  TABLE_TYPE = 'BASE TABLE'
+--SELECT @sql += ' Drop table ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '
+--FROM   INFORMATION_SCHEMA.TABLES
+--WHERE  TABLE_TYPE = 'BASE TABLE'
+
+DECLARE @sql NVARCHAR(MAX) = N'';
+
+SELECT @sql += N'
+  DROP DATABASE ' + QUOTENAME(name)
+  + N';' 
+FROM sys.databases
+WHERE name NOT IN (N'master',N'tempdb',N'model',N'msdb');
+
+PRINT @sql;
+-- EXEC master.sys.sp_executesql @sql;
